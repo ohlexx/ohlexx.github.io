de10d5bd96ff6cb0304dc902d5aa90961e54be3c
Yunseong Oh <alex.yunseong@gmail.com>
2023-01-24 13:18:56 +0900

    update my skiils

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+const lang = [python, html, css, js, c];
 
+const codes = document.querySelector("#codes");
+const showlang = lang[Math.floor(Math.random() * lang.length)];
+
+codes.innerText = showlang.codes;
